f8288f90e7a13240bc795783e8187322699ffaae
Kaitlyn Dodds <kaitlyndodds@Admins-MacBook-Pro.local>
2018-02-06 16:47:44 -0700

    Added styling to show page for campgrounds

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var seedDB = require('./seeds');
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 
